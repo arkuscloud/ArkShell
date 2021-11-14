0ccd0683fec21b197dc6ec72c1f03cfa43c7f345
Solan <solan@arkuscloud.io>
2021-11-13 23:27:07 -0500

    ArkShell Function Template
    Comment Help + First version of function template

---
+++ ArkShell Sessions/ArkShell -0_Template.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+
+.PARAMETER Name
+
+
+.PARAMETER Extension
+
+
+.INPUTS
+
+
+.OUTPUTS
+
+
+.EXAMPLE
+
+
+.EXAMPLE
+
+
+.EXAMPLE
+
+
+.LINK
+
+https://arkuscloud.io
+
+.LINK
+
+#>
+
+
+function Add-Extension{
+    param ([string]$Name,[string]$Extension = "txt")
+    $name = $name + "." + $extension
+    $name
+}
