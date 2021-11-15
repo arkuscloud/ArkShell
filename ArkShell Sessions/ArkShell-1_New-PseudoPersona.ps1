e3823201c19239d7bc2f5e39d52a800983ea4c22
Solan <solan@arkuscloud.io>
2021-11-14 21:21:37 -0500

    Create ArkShell 1 _ New-PseudoPersona.ps1
    New-PseudoPersona Function

---
+++ ArkShell Sessions/ArkShell-1_New-PseudoPersona.ps1
@@ -0,0 +1,98 @@
+<#
+##########################################
+##                                      ##
+##    ArkusCloud.io | ArkShell [ 1 ]    ##
+##                                      ##
+##########################################
+
+.SYNOPSIS
+Create one or more pseudo users with Active Directory user properties.
+
+.DESCRIPTION
+
+- > New-PseudoPersona function will create one or more pseudo users. 
+- > These users will be sent to .csv file in your local C:\ Transcript directory.
+- > All errors will be sent to your local C:\ Error directory
+
+
+.NOTES
+File Name : ArkShell #1 - New-PseudoPersona.ps1
+Author    : Solan White
+Requires  : PowerShell Version 3.0 or above
+
+.LINK
+https://www.arkuscloud.io
+
+#>
+
+function New-PseudoPersona {
+    [CmdletBinding()]
+    param (
+        $Transcriptdir  = "c:\Users\$env:username\ArkShell_Files\New-PseudoPersona\Transcript", 
+        $Errordir       = "c:\Users\$env:username\ArkShell_Files\New-PseudoPersona\Error",
+        $CSVdir         = "c:\Users\$env:username\ArkShell_Files\New-PseudoPersona\.csv",
+        $ErrorLog       = "c:\Users\$env:username\ArkShell_Files\New-PseudoPersona\Error\New-PseudoPersona_Error $( get-date -Format MM-dd-yy_HHmm-ss ).txt",
+        $NewReport      = "c:\Users\$env:username\ArkShell_Files\New-PseudoPersona\.csv\New-PseudoPersona_Report $( get-date -Format MM-dd-yy_HHmm-ss ).csv",
+        # NameIT -Count Parameter
+        [Parameter(Mandatory,Position=0)]
+        [int] $Count 
+    )
+    
+    begin {
+        if(!(Test-Path -Path $Transcriptdir )){
+            New-Item -ItemType directory -Path $Transcriptdir
+            Write-Host "Transcript Directory created"
+        }
+        else{ 
+            Write-Host "Transcript Directory exists"
+        }
+        if(!(Test-Path -Path $Errordir )){
+            New-Item -ItemType directory -Path $Errordir
+            Write-Host "Error Directory created"
+        }
+        else{ 
+            Write-Host "Error Directory exists"
+        }
+        if(!(Test-Path -Path $CSVdir )){
+            New-Item -ItemType directory -Path $CSVdir
+            Write-Host ".CSV Directory created"
+        }
+        else{ 
+            Write-Host ".CSV Directory exists"
+        }
+        Start-Transcript -Path "$Transcriptdir\New-PseudoPersona_Transcript $( get-date -Format MM-dd-yy_HHmm-ss ).txt" -Verbose
+        $Template = @"
+        FirstName = [person female first]
+        LastName = [person female last]
+        DisplayName = $CustomData = @{}
+        Street = [address]
+        State = [state abbr]
+        City = [city]
+        Zip = [numeric][numeric][numeric][numeric][numeric]
+        Telephone = [numeric][numeric][numeric]-[numeric][numeric][numeric]-[numeric][numeric][numeric][numeric]
+        Hire Date = [randomdate]
+        Password = #@#??@#?#?!#??*#?%
+        Country = USA
+        Company = Arkus Cloud
+"@
+    }
+    
+    process {
+        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+        
+        Install-Module -Name NameIT -Verbose -Confirm:$false 
+
+        $Results = Invoke-Generate $Template -Count $Count -AsPSObject
+
+        $Results
+
+        $Results | Export-Csv -Path $NewReport -Append 2>> $ErrorLog -NoTypeInformation
+
+    }
+    
+    end {
+        
+        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
+        Stop-Transcript
+    }
+}
