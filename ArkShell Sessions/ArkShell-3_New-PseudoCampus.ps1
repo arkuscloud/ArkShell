9af28e308819510065b1e9098b6d48a33aa50d72
Solan <solan@arkuscloud.io>
2021-11-16 21:32:19 -0500

    Update ArkShell 3 - Spacing
    Update ArkShell 3 - Spacing

--- ArkShell Sessions/ArkShell-3_New-PseudoCampus.ps1
+++ ArkShell Sessions/ArkShell-3_New-PseudoCampus.ps1
@@ -38,44 +38,71 @@ function New-PseudoCampus {
     )
     
     begin {
+
         if(!(Test-Path -Path $Transcriptdir )){
+
             New-Item -ItemType directory -Path $Transcriptdir
+
             Write-Host "Transcript Directory created"
+
         }
+
         else{ 
+
             Write-Host "Transcript Directory exists"
+
         }
+
         if(!(Test-Path -Path $Errordir )){
+
             New-Item -ItemType directory -Path $Errordir
+
             Write-Host "Error Directory created"
+
         }
+
         else{ 
+
             Write-Host "Error Directory exists"
+
         }
+
         if(!(Test-Path -Path $CSVdir )){
+
             New-Item -ItemType directory -Path $CSVdir
+
             Write-Host ".CSV Directory created"
+
         }
+
         else{ 
+
             Write-Host ".CSV Directory exists"
+
         }
+
         Start-Transcript -Path "$Transcriptdir\New-PseudoCampus_Transcript $( get-date -Format MM-dd-yy_HHmm-ss ).txt" -Verbose
+
         $Campus = @"
-        Charter     = [state]-##?
-        P.O. Box.   = 'PO Box #####'
+        Charter     = [cmdlet][space]##
+        POC         = [person]
+        P.O. Box    = PO Box #####
         Street      = [address]
         State       = [state abbr]
         City        = [city]
         Zip         = 5[numeric][numeric][numeric][numeric]
-        Telephone   = 301-9[numeric][numeric]-9[numeric][numeric][numeric]
+        Telephone   = 301-7[numeric][numeric]-4[numeric][numeric][numeric]
         Country     = USA
-        POC         = [person]
+        Charter ID  = [guid]
 "@
-        
+        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+                
+        Install-Module -Name NameIT -Verbose -Confirm:$false 
     }
     
     process {
-        $Results = Invoke-Generate $Campus -Count $Count -AsPSObject | Select-Object POC, Charter, Street, City, State, Zip, Telephone, Country
+
+        $Results = Invoke-Generate $Campus -Count $Count -AsPSObject #| Select-Object Charter, POC, P.O. Box, Street, City, State, Zip, Telephone, Country, 'Charter ID'
 
         $Results
 
@@ -86,6 +113,9 @@ function New-PseudoCampus {
     end {
         
         Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
+
         Stop-Transcript
+
     }
+    
 }
