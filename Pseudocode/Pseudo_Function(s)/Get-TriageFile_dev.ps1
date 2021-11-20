2a1526c3a058a6aec52c2c57f0040f37d938c326
Solan <solan@arkuscloud.io>
2021-11-20 07:52:00 -0500

    Update Get-TriageFile
    Added $Today variable; Directory loop(s)

--- Pseudocode/Pseudo_Function(s)/Get-TriageFile_dev.ps1
+++ Pseudocode/Pseudo_Function(s)/Get-TriageFile_dev.ps1
@@ -37,13 +37,16 @@ function Get-TriageFile {
     
     [CmdletBinding()]
     param (
-        $Triagedir      = "c:\Users\$env:username\ArkShell_Files\Get-TriageZone\Triage",
-        $Launchdir      = "c:\Users\$env:username\ArkShell_Files\Get-TriageZone\Launch",
-        $Transcriptdir  = "c:\Users\$env:username\ArkShell_Files\Get-TriageZone\Transcript", 
-        $Errordir       = "c:\Users\$env:username\ArkShell_Files\Get-TriageZone\Error",
-        $CSVdir         = "c:\Users\$env:username\ArkShell_Files\Get-TriageZone\.csv",
-        $ErrorLog       = "c:\Users\$env:username\ArkShell_Files\Get-TriageZone\Error\Get-TriageZone_Error $( get-date -Format MM-dd-yy_HHmm-ss ).txt",
-        $NewReport      = "c:\Users\$env:username\ArkShell_Files\Get-TriageZone\.csv\Get-TriageZone_Report $( get-date -Format MM-dd-yy_HHmm-ss ).csv"
+        $Triagedir      = "c:\Users\$env:username\ArkShell_Files\UserTriageZone",
+        $Launchdir      = "c:\Users\$env:username\ArkShell_Files\UserLaunchPad",
+        $Transcriptdir  = "c:\Users\$env:username\ArkShell_Files\Get-TriageFile\Transcript", 
+        $Errordir       = "c:\Users\$env:username\ArkShell_Files\Get-TriageFile\Error",
+        $CSVdir         = "c:\Users\$env:username\ArkShell_Files\Get-TriageFile\.csv",
+        $ErrorLog       = "c:\Users\$env:username\ArkShell_Files\Get-TriageFile\Error\Get-TriageFile_Error $( get-date -Format MM-dd-yy_HHmm-ss ).txt",
+        $NewReport      = "c:\Users\$env:username\ArkShell_Files\Get-TriageFile\.csv\Get-TriageFile_Report $( get-date -Format MM-dd-yy_HHmm-ss ).csv",
+        $LaunchReport   = "c:\Users\$env:username\ArkShell_Files\UserLaunchPad\UserOnboard_Report $( get-date -Format MM-dd-yy_HHmm-ss ).csv",
+        $TriageReport   = "c:\Users\$env:username\ArkShell_Files\UserTriageZone\UserOnboard_Report $( get-date -Format MM-dd-yy_HHmm-ss ).csv",
+        $Today          = (Get-Date).Date
     )
     
     begin {
@@ -53,6 +56,13 @@ function Get-TriageFile {
         }
         else{ 
             Write-Host "Launch Directory exists"
+        }
+        if(!(Test-Path -Path $Triagedir )){
+            New-Item -ItemType directory -Path $Triagedir
+            Write-Host "Triage Directory created"
+        }
+        else{ 
+            Write-Host "Triage Directory exists"
         }
         if(!(Test-Path -Path $Transcriptdir )){
             New-Item -ItemType directory -Path $Transcriptdir
@@ -75,13 +85,22 @@ function Get-TriageFile {
         else{ 
             Write-Host ".CSV Directory exists"
         }
+        Start-Transcript -Path "$Transcriptdir\Get-TriageFile_Transcript $( get-date -Format MM-dd-yy_HHmm-ss ).txt" -Verbose
     }
     
     process {
-        
+        $UserDumpdir    = "c:\Users\$env:username\ArkShell_Files\UserDump"
+        #Get UserDump File
+        Get-ChildItem -Path $UserDumpdir | Where-Object { $_.LastWriteTime -ge $Today -and $_.LastWriteTime -lt $Today.AddDays(1) } | Move-Item -Destination $Triagedir;
+
+        #Get Triage File
+        #Get-ChildItem -Path $UserDumpdir | Where-Object { ($_.LastWriteTime -ge $Today) }
+
+        #Import-Csv
+
     }
     
     end {
-        
+        Stop-Transcript
     }
 }
